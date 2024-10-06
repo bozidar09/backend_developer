fcfb509069ec5f55b0130bc29c3c0d3ab094240e
bozidar <bozidar.markovic09@mail.com>
2024-10-07 01:32:46 +0200

    views service updated

--- laravel-cms/app/Services/ViewCounterService.php
+++ laravel-cms/app/Services/ViewCounterService.php
@@ -6,9 +6,17 @@ use App\Models\Article;
 
 class ViewCounterService
 {
-    public function count(Article $article) 
+    public function count(Article $article): void
     {
-        // TODO session/timer validation
+        if (request()->session()->has('views')) {
+            $views = request()->session()->get('views');
+            if (array_key_exists($article->id, $views)) {
+                if($views[$article->id]['created_at']->diffInSeconds(now()) < 60) {
+                    return;
+                }
+            }
+        }
+        request()->session()->put($views[$article->id]['created_at'], now());
         $article->increment('views');
     }
 }
