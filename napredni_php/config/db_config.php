0ea64e37074279f37aa6270822ddbe233cc7c847
bozidar <bozidar.markovic09@mail.com>
2024-11-02 01:30:25 +0100

    config refactor

--- napredni_php/config/db_config.php
+++ napredni_php/config/db_config.php
@@ -7,19 +7,22 @@ $config = [
     'db_charset' => env('DB_CHARSET', "utf8mb4"),
     'db_username' => env('DB_USERNAME', "algebra"),
     'db_password' => env('DB_PASSWORD', "algebra"),
-    'db_options' => env('DB_OPTIONS'),
+    'db_options' => [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ],
 ];
 
-if ($config['db_options']) {
-    if (str_contains($config['db_options'] , ',')) {
-        $elements = explode(',', $config['db_options']);
-    } else {
-        $elements[] = $config['db_options'];
-    }
+// if ($config['db_options']) {
+//     if (str_contains($config['db_options'] , ',')) {
+//         $elements = explode(',', $config['db_options']);
+//     } else {
+//         $elements[] = $config['db_options'];
+//     }
     
-    $config['db_options'] = [];
-    foreach ($elements as $element) {
-        $element = explode('=>', $element);
-        $config['db_options'][$element[0]] = $element[1];
-    }
-}
+//     $config['db_options'] = [];
+//     foreach ($elements as $element) {
+//         $element = explode('=>', $element);
+//         $config['db_options'][$element[0]] = $element[1];
+//     }
+// }
